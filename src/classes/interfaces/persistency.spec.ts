de925266e5f6612d3e60281c388dde000ba2ffc2
lskwr <lskwr@gmail.com>
2024-02-05 18:35:41 -0300

    typescript aula 355 - asserçoes com o jest

--- src/classes/interfaces/persistency.spec.ts
+++ src/classes/interfaces/persistency.spec.ts
@@ -1,14 +1,13 @@
-describe('TESTANDO ALGUMA COISA',() => {
+describe('TESTANDO ALGUMA COISA', () => {
   it('should return 1', () => {
     const number = 1;
     expect(number).toBe(1);
   });
-})
+});
 
 describe('TESTANDO OUTRA COISA', () => {
-  test('should return Luiz',() => {
+  test('should return Luiz', () => {
     const nome = 'Luiz';
     expect(nome).not.toBe('Luiz');
   });
-})
-
+});
---
+++ src/examples/test-jest-assertions.spec.ts
@@ -0,0 +1,39 @@
+describe('Primitive values', () => {
+  it('should test jest assertions', () => {
+    const number = 10;
+    expect(number).toBeLessThan(11);
+    expect(number).toBeLessThanOrEqual(10);
+
+    expect(number).toBeCloseTo(10.001);
+    expect(number).toBeCloseTo(9.996);
+
+    expect(number).toHaveProperty('toString'); //checa se number tem a propriedade toString
+  });
+  it('should split tests', () => {
+    const number = 10;
+
+    expect(number).toBe(10);
+    expect(number).toEqual(10);
+
+    expect(number).not.toBeNull();
+    expect(number).not.toBeFalsy();
+    expect(number).toBeTruthy();
+    expect(number).toBeGreaterThan(9);
+    expect(number).toBeGreaterThanOrEqual(10);
+  });
+});
+
+describe('Objects', () => {
+  it('should test jest assertions with objects', () => {
+    const person = { name: 'Luiz', age: 44 };
+    const anotherPerson = { ...person };
+
+    expect(person).toBe(anotherPerson);
+    expect(person).toEqual(anotherPerson);
+    expect(person).toHaveProperty('age');
+    expect(person).toHaveProperty('age', 30);
+    expect(person).not.toHaveProperty('lastname');
+
+    expect(person.name).toBe('Luiz');
+  });
+});
