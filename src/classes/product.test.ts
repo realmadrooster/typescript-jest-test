0347d3e5de8fa6ff5a7806390685c818915289b3
lskwr <lskwr@gmail.com>
2024-02-08 18:31:23 -0300

    typescript aula 358 testando product.ts

---
+++ src/classes/product.test.ts
@@ -0,0 +1,15 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price);
+};
+
+describe('Messaging', () => {
+  afterEach(() => jest.clearAllMocks()); //limpa os mocks depois de cada teste
+  it('should returned undefined', () => {
+    //system under test
+    const sut = createSut('camiseta', 49.9);
+    expect(sut).toHaveProperty('name', 'camiseta');
+    expect(sut.price).toBeCloseTo(49.9); //para testar valores de ponto flutuante use o toBeCloseTo
+  });
+});
