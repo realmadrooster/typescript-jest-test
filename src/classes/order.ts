4995ae85a71e9c9c3d46c6ea98d8a5eefc841dee
lskwr <lskwr@gmail.com>
2024-02-19 15:18:54 -0300

    typescript aula 364 test coverage (cobertura de testes)

--- src/classes/order.ts
+++ src/classes/order.ts
@@ -1,22 +1,20 @@
-import { OrderStatus } from "./interfaces/order-status";
-import { CustomerOrder } from "./interfaces/customer-protocol";
-import { ShoppingCartProtocol } from "./interfaces/shopping-cart-protocol";
-import { MessagingProtocol } from "./interfaces/messagin-protocol";
-import { PersistencyProtocol } from "./interfaces/persistency-protocol";
-
+import { OrderStatus } from './interfaces/order-status';
+import { CustomerOrder } from './interfaces/customer-protocol';
+import { ShoppingCartProtocol } from './interfaces/shopping-cart-protocol';
+import { MessagingProtocol } from './interfaces/messagin-protocol';
+import { PersistencyProtocol } from './interfaces/persistency-protocol';
 
 export class Order {
-    private _orderStatus: OrderStatus = 'open';
+  private _orderStatus: OrderStatus = 'open';
 
   constructor(
     private readonly cart: ShoppingCartProtocol,
     private readonly messaging: MessagingProtocol,
     private readonly persistency: PersistencyProtocol,
     private readonly customer: CustomerOrder,
-    ) {}
+  ) {}
 
-
-    get orderStatus(): OrderStatus {
+  get orderStatus(): OrderStatus {
     return this._orderStatus;
   }
 
@@ -28,10 +26,16 @@ export class Order {
     }
 
     this._orderStatus = 'closed';
-    this.messaging.sendMessage(`seu pedido com total de ${this.cart.totalWithDiscount()} foi recebido`);
+    this.messaging.sendMessage(
+      `seu pedido com total de ${this.cart.totalWithDiscount()} foi recebido`,
+    );
     this.persistency.saveOrder();
     this.cart.clear();
-    console.log('O cliente é :', this.customer.getName(), this.customer.getIDN(),);
+    console.log(
+      'O cliente é :',
+      this.customer.getName(),
+      this.customer.getIDN(),
+    );
   }
 
   //  sendMessage(msg: string): void {
